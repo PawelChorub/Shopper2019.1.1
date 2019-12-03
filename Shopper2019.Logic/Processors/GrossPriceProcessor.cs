5a0b6aa9bcc81c36ed3029ade52d1518b52367cb
Paweł <pawelchorub@gmail.com>
2019-12-03 10:11:53 +0100

    remove redundant from GrossPriceProcessoe class

--- Shopper2019.Logic/Processors/GrossPriceProcessor.cs
+++ Shopper2019.Logic/Processors/GrossPriceProcessor.cs
@@ -9,12 +9,9 @@ namespace Shopper2019.Logic.Processors
 {
     public class GrossPriceProcessor
     {
-        public static decimal GrossPrice { get; set; } 
-
         public static decimal GrossPriceCalculate(decimal netPrice, int vatValue)
         {
-
-            GrossPrice = netPrice + ((netPrice / 100) * vatValue);
+            decimal GrossPrice = netPrice + ((netPrice / 100) * vatValue);
 
             return GrossPrice;
         }
