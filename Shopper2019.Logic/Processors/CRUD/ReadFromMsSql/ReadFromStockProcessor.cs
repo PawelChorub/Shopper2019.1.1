652428bed6f3bba109bf67e413a6a122ae7575f3
Paweł <pawelchorub@gmail.com>
2019-12-03 16:30:32 +0100

    delete unusable code

--- Shopper2019.Logic/Processors/CRUD/ReadFromMsSql/ReadFromStockProcessor.cs
+++ Shopper2019.Logic/Processors/CRUD/ReadFromMsSql/ReadFromStockProcessor.cs
@@ -40,7 +40,7 @@ namespace Shopper2019.Logic.Processors
                 {
                     item.Code = (reader["code"].ToString());
                     item.Name = (reader["name"].ToString());
-                    
+                   
                     item.SaleQuantity = (decimal)reader["quantity"];
                     item.UnitOfMeasurements = (reader["unitOfMeasurement"].ToString());
                     item.VatValue = (Int32.Parse(reader["vatTax"].ToString()));
@@ -65,48 +65,6 @@ namespace Shopper2019.Logic.Processors
             }
                 
             else return null;
-            //double stockQuantityAvailable = 0;
-            //ISaleItem item = Factory.CreateSaleItem();
-            //ISaleItem outputItem = Factory.CreateSaleItem();
-
-            //sqlConnection.Open();
-            //sqlQuery = string.Format("select * from Shopper2019StockTable WHERE code = '{0}'", itemCode);
-            //sqlCommand = new SqlCommand(sqlQuery, sqlConnection);
-            //reader = sqlCommand.ExecuteReader();
-
-            //if (reader.HasRows)
-            //{
-            //    while (reader.Read())
-            //    {
-            //        item.Code = (reader["code"].ToString());
-            //        item.Name = (reader["name"].ToString());
-
-            //        stockQuantityAvailable = (double)reader["quantity"];
-
-            //        if (itemSearchQuantity <= stockQuantityAvailable)
-            //        {
-            //            item.SaleQuantity = itemSearchQuantity;
-            //        }
-            //        else
-            //        {
-            //            item.SaleQuantity = 0;
-            //        }
-
-            //        item.VatValue = (Int32.Parse(reader["vatTax"].ToString()));
-            //        item.Gross_Price = (double)reader["grossPrice"];
-
-            //        //UnitOfMeasureGet = (reader["unitOfMeasure"].ToString());
-            //    }
-            //}
-            //sqlConnection.Close();
-
-            ////zabezpieczenie znalezienia i ilości
-            //if ((itemCode == item.Code) && (itemSearchQuantity == item.SaleQuantity))
-            //    outputItem = item;
-
-            //else outputItem = null;
-
-            
         }
 
     }
