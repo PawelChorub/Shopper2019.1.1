cd3cb30c0a7501d8e57ed304885326378de5189c
Paweł <pawelchorub@gmail.com>
2019-12-03 16:24:25 +0100

    Create TotalPriceValueProcessor_Test to test calculating method, test passed

--- Shopper2019_Test/TotalPriceValueProcessor_Test.cs
+++ Shopper2019_Test/TotalPriceValueProcessor_Test.cs
@@ -10,6 +10,15 @@ namespace Shopper2019.Test
 {
     public class TotalPriceValueProcessor_Test
     {
+        [Theory]
+        [InlineData(13.01, 25, 325.25)]
+        [InlineData(21.25, 3.65, 77.56)]
+        [InlineData(2569.369, 235.478, 605029.87)]
+        public void CalculateTotalPriceValue_ShouldCalculateInPrecisionOfDecimalPlacesTwo(decimal _quantity, decimal _price, decimal expected)
+        {
+            decimal actual = TotalPriceValueProcessor.CalculateTotalPriceValue(_quantity, _price);
 
+            Assert.Equal(expected, actual, 2);
+        }
     }
 }
